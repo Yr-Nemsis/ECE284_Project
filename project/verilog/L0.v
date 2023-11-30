b206a482f86cdcc383fe6c71b64b2ede1e2c2fec
Janav Kati <janav7777@gmail.com>
2023-11-29 19:12:09 -0800

    update L0

--- project/verilog/L0.v
+++ project/verilog/L0.v
@@ -1,71 +1,66 @@
-module L0(clk, wr, rd, reset, in, out, o_full, o_ready);
+// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
+// Please do not spread this code without permission 
+module l0 (clk, in, out, rd, wr, o_full, reset, o_ready);
 
-  parameter col  = 8;
+  parameter row  = 8;
   parameter bw = 4;
 
   input  clk;
   input  wr;
   input  rd;
   input  reset;
-  input  [bw*col-1:0] in;
-  output [bw*col-1:0] out;
+  input  [row*bw-1:0] in;
+  output [row*bw-1:0] out;
   output o_full;
   output o_ready;
 
-  wire [col-1:0] empty;
-  wire [col-1:0] full;
-  reg  rd_en;
+  wire [row-1:0] empty;
+  wire [row-1:0] full;
+  reg [row-1:0] rd_en;
   
   genvar i;
+  integer ii;
 
-  wire [col-1:0] fifo_full;
-  wire [col-1:0] fifo_empty;
+  assign o_full  =  |full; // any fifo is full
+  assign o_ready =  ~o_full; // all fifos are not full
 
-  reg [col-1:0] rd_temp;
-
-  reg [bw*col-1:0] out_temp;
-  wire [bw*col-1:0] out_wire;
-
-  assign o_ready = (|fifo_full)? 1'b0: 1'b1; // when all fifo's are not full
-  assign o_full  = ~o_ready ; // When any fifo is full 
-
-  for (i=0; i<col ; i=i+1) begin : col_num
-      fifo_depth8 #(.bw(bw)) fifo_instance (
-	      .rd_clk(clk),
-	      .wr_clk(clk),
-	      .rd(rd_temp[i]),
-	      .wr(wr),
-        .o_empty(fifo_empty[i]),
-        .o_full(fifo_full[i]),
-	      .in(in[bw*(i+1)-1:bw*i]),
-	      .out(out_wire[bw*(i+1)-1:bw*i]),
-        .reset(reset)
-      );
+  for (i=0; i<row ; i=i+1) begin : row_num
+    fifo_depth8 #(.bw(bw)) fifo_instance (
+      .rd_clk(clk),
+      .wr_clk(clk),
+      .rd(rd_en[i]),
+      .wr(wr),
+      .o_empty(empty[i]),
+      .o_full(full[i]),
+      .in(in[(bw*(i+1))-1:bw*(i)]),
+      .out(out[(bw*(i+1))-1:bw*(i)]),
+      .reset(reset)
+    );
   end
 
 
   always @ (posedge clk) begin
-    if(reset)
-    begin
-      rd_temp <= 'b0;
+   if (reset) begin
+      rd_en <= 8'b00000000;
+   end
+   else
+
+      /////////////// version1: read all row at a time ////////////////
+      // for(ii = 0; ii < row; ii = ii +1)
+      // begin
+      //   rd_en[ii] <= rd;
+      // end
+      ///////////////////////////////////////////////////////
+
+
+
+      //////////////// version2: read 1 row at a time /////////////////
+      rd_en[0] <= rd;
+      for(ii = 1; ii < row; ii = ii+1)
+      begin
+        rd_en[ii] <= rd_en[ii-1];
+      end 
+      ///////////////////////////////////////////////////////
     end
-    else
-    begin
-      rd_temp[0] <= rd; 
-      rd_temp[1] <= rd_temp[0]; 
-      rd_temp[2] <= rd_temp[1]; 
-      rd_temp[3] <= rd_temp[2]; 
-      rd_temp[4] <= rd_temp[3]; 
-      rd_temp[5] <= rd_temp[4]; 
-      rd_temp[6] <= rd_temp[5]; 
-      rd_temp[7] <= rd_temp[6]; 
-
-      out_temp <= out_wire;
-    end
-
-  end
-
-  assign out = out_temp;
-
 
 endmodule
--- project/verilog/corelet.v
+++ project/verilog/corelet.v
@@ -1,4 +1,4 @@
-module corelet(clk, reset, in, out, inst);
+module corelet(clk, reset, in, out, inst, l0_status);
 
   parameter bw = 4;
   parameter psum_bw = 16;
@@ -9,21 +9,21 @@ module corelet(clk, reset, in, out, inst);
   input [33:0] inst;
   input [col*bw-1:0] in;
   input [col*bw-1:0] out;
-
+  output [1:0] l0_status; // l0_status[0] => full, l0_status_[1] => ready
 
 
 
   // inst[4] == 1 then read from L0
   // inst[5] == 1 write to L0
-  L0 #(.bw(bw)) L0_instance(
+  l0 #(.bw(bw), .row(row)) L0_instance(
     .clk(clk),
     .wr(inst[5]),
     .rd(inst[4]),
     .reset(reset),
     .in(in),
     .out(out),
-    .o_full(),
-    .o_ready()
+    .o_full(l0_status[0]),
+    .o_ready(l0_status[1])
   );
 /*
   mac_array #(.bw(bw), .psum_bw(psum_bw)) mac_array_instance(
